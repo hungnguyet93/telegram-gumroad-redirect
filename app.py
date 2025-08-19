ea05319cbbdb5d3be00860f1dcdb2ca24b0479c6
hungnguyet <hungnguyet1504@gmail.com>
2025-08-19 16:33:39 +0700

    Initial commit

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ app = Flask(__name__)
 # Route trang chủ
 @app.route("/")
 def home():
-    return "Hello from Render + Flask!"
+    return "Hello from Flask!"
 
 # Route '/join' để xử lý tham số 'k' từ URL
 @app.route("/join")
 def join():
     k = request.args.get('k')  # Lấy tham số 'k' từ URL
     if k:
-        return f"Chào bạn, kênh Telegram của bạn là: {k}"  # Hiển thị giá trị của tham số k
+        return f"Chào bạn, kênh Telegram của bạn là: {k}"
     else:
         return "Không có mã kênh!"  # Nếu không có tham số k, trả về thông báo này
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
